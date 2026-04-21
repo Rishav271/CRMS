e8e1a9706362e37db88b9c918b7bc746bd5bfe7d
Rishav Raj Gupta <rishavrajgupta6@gmail.com>
2026-04-21 13:41:32 +0530

    Update settings.py

--- college_events/college_events/settings.py
+++ college_events/college_events/settings.py
@@ -74,12 +74,13 @@ WSGI_APPLICATION = 'college_events.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('VERCEL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
